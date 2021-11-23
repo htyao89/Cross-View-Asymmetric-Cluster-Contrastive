a1f25f2c7370edd52efd12dc8b2ed4ca6478f57d
htyao89 <74002548+coldrainyht@users.noreply.github.com>
2021-11-23 16:07:41 +0800

    Update run_market_unsupervised.sh

--- run_market_unsupervised.sh
+++ run_market_unsupervised.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=2,3  python examples/main_unsupervised.py -b 128 -a resnet50 -d market1501 --iters 400 --w 0.5 --momentum 0.1 --eps 0.4 --num-instances 16  --logs-dir ./example_final/market_unsupervised
+CUDA_VISIBLE_DEVICES=0,1  python examples/main_unsupervised.py -b 128 -a resnet50 -d market1501 --iters 400 --w 0.5 --momentum 0.1 --eps 0.4 --num-instances 16  --logs-dir ./example_final/market_unsupervised
 
 
