8f6e490ef8d95f994ce3528c44b363a7d0bfa87b
yaohanzhigu <hantao.yao@ia.ac.cn>
2021-11-23 17:04:24 +0800

    Update run_market_supervised.sh

--- run_market_supervised.sh
+++ run_market_supervised.sh
@@ -1,1 +1,1 @@
-python examples/main.py -b 128 -a resnet50 -d market1501 --momentum 0.1 --w 0.0 --num-instances 16 --logs-dir ./examples_cvpr/market1501_supervised
+python examples/main.py -b 128 -a resnet50 -d market1501 --momentum 0.1 --w 0.25 --num-instances 16 --logs-dir ./examples_cvpr/market1501_supervised
