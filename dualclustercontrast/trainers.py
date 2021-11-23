cdb8ccc2722776004580d045244a05a286ada99f
htyao89 <74002548+coldrainyht@users.noreply.github.com>
2021-11-23 15:24:20 +0800

    Create trainers.py

---
+++ dualclustercontrast/trainers.py
@@ -0,0 +1,57 @@
+from __future__ import print_function, absolute_import
+import time
+from .utils.meters import AverageMeter
+
+
+class DualClusterContrastTrainer(object):
+    def __init__(self, encoder, loss=None):
+        super(DualClusterContrastTrainer, self).__init__()
+        self.encoder = encoder
+        self.loss = loss
+
+    def train(self, epoch, data_loader, optimizer, print_freq=10):
+        self.encoder.train()
+
+        batch_time = AverageMeter()
+        data_time = AverageMeter()
+
+        losses = AverageMeter()
+
+        end = time.time()
+        for i, inputs in enumerate(data_loader):
+            data_time.update(time.time() - end)
+
+            # process inputs
+            inputs, labels, indexes = self._parse_data(inputs)
+
+            # forward
+            f_out = self._forward(inputs)
+            loss = self.loss(f_out, labels)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            losses.update(loss.item())
+
+            # print log
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            if (i + 1) % print_freq == 0:
+                print('Epoch: [{}][{}/{}]\t'
+                      'Time {:.3f} ({:.3f})\t'
+                      'Data {:.3f} ({:.3f})\t'
+                      'Loss {:.3f} ({:.3f})'
+                      .format(epoch, i + 1, len(data_loader),
+                              batch_time.val, batch_time.avg,
+                              data_time.val, data_time.avg,
+                              losses.val, losses.avg))
+
+    def _parse_data(self, inputs):
+        imgs, _, pids, _, indexes = inputs
+        return imgs.cuda(), pids.cuda(), indexes.cuda()
+
+    def _forward(self, inputs):
+        return self.encoder(inputs)
+
