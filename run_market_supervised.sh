53d11b908401514319c351a4b5cba668ebc6d1c3
htyao89 <74002548+coldrainyht@users.noreply.github.com>
2021-11-23 15:37:55 +0800

    Update run_market_supervised.sh

--- run_market_supervised.sh
+++ run_market_supervised.sh
@@ -1,1 +1,2 @@
-
+python examples/main.py -b 128 -a resnet50 -d market1501 --momentum 0.1 --num-instances 16 --logs-dir ./examples_cvpr/market1501_supervised
+#python examples/main_cvpr.py -b 128 -a resnet_ibn50a -d personx  --iters 46 --momentum 0.1 --eps 0.4 --num-instances 16 --logs-dir ./examples_cvpr/personX_resnet_ibn50a
